ce81c092f10dbae13de685a755166ee9145a5be4
NMAP327 <nadyamelania27@gmail.com>
2023-04-17 12:00:28 +0700

    first commit

---
+++ resources/views/first.blade.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> 
+   
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+ 
+  <div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="images/nds-logo.jpg" alt="" width="150" height="100">
+    <h1 class="display-5 fw-bold">NMAP Driving Schools</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4"> NMAP Driving Schools is a structured driving lesson programme with professional driver training from Bandung's leading driving school.</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <a href="/home" class="btn btn-info" role="button">Get Started</a>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
+    
+<main>
+  
+  
